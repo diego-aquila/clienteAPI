7de6d9314fb78e02474279f9874f39f1ff45018d
diego.aasampaio <diego.aasampaio@TIT0676461W11-1>
2025-10-17 19:42:50 -0300

    Adicionar arquivos de projeto.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using clienteAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace clienteAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Cliente> Clientes { get; set; }
+       
+        }
+}
---
+++ Models/Cliente.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace clienteAPI.Models
+{
+    public class Cliente
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="O nome é um dado obrigatório")]
+        [StringLength(200, MinimumLength = 3, ErrorMessage = "O nome deve ter entre 3 e 200 caracteres")]
+        public string Nome { get; set; } = string.Empty;
+
+        [Required(ErrorMessage ="O email é obrigatório")]
+        [EmailAddress(ErrorMessage ="Email em formato inválido")]
+        [StringLength(100, ErrorMessage ="O email deve ter no máximo 100 caracteres")]
+        public string Email { get; set; } = string.Empty;
+
+        public string Telefone { get; set; } = string.Empty;
+
+        [DataType(DataType.DateTime)]
+        public DateTime DataCadastro { get; set; } = DateTime.Now;
+    }
+}
